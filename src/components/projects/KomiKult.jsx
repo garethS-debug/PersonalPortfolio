08f733a2051084fea17899834f779d17a08b51a2
GarethSwarte <garethjswarte@gmail.com>
2026-03-18 12:10:23 +0000

    Moving projects to a sub folder

---
+++ src/components/projects/KomiKult.jsx
@@ -0,0 +1,17 @@
+import { komikult } from '../../assets';
+
+const KomiKult = {
+  id: 'project-1',
+  name: 'KomiKult',
+  description: 'A comic characters list app that displays Marvel characters.',
+  tags: [
+    { name: 'react', color: 'blue-text-gradient' },
+    { name: 'mongodb', color: 'green-text-gradient' },
+    { name: 'tailwind', color: 'pink-text-gradient' },
+  ],
+  image: komikult,
+  repo: 'https://github.com/shaqdeff/KomiKult',
+  demo: 'https://shaqdeff.github.io/KomiKult/',
+};
+
+export default KomiKult;
