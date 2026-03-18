345adb2cefe5589d1210b78a936940620fc7ccaa
GarethSwarte <garethjswarte@gmail.com>
2026-03-18 23:58:32 +0000

    Updating files with experience

--- src/components/filesystem/FilesDemo.jsx
+++ src/components/filesystem/FilesDemo.jsx
@@ -19,97 +19,46 @@ import {
   FolderIcon as LucideFolderIcon,
   FileIcon as LucideFileIcon,
 } from 'lucide-react';
+import filesData from './filesData';
 
 export const FilesDemo = () => {
   return (
     <div className="dark relative size-full border bg-background overflow-auto text-foreground">
-      <Files className="p-3" defaultOpen={["folder-1"]}>
+      <Files className="p-3" defaultOpen={[`folder-0`]}>
         <FilesHighlight className="bg-accent pointer-events-none">
-          <FolderItem value="folder-1">
-            <FolderHeader>
-              <FolderTrigger className="w-full text-start">
-                <FolderHighlight>
-                  <Folder className="flex items-center gap-2 p-2 pointer-events-none">
-                    <FolderIcon
-                      closeIcon={<LucideFolderIcon className="size-4.5 text-foreground" />}
-                      openIcon={<FolderOpenIcon className="size-4.5 text-foreground" />}
-                    />
-                    <FileLabel className="text-sm">Folder 1</FileLabel>
-                  </Folder>
-                </FolderHighlight>
-              </FolderTrigger>
-            </FolderHeader>
+          {filesData.map((folder, i) => (
+            <FolderItem value={`folder-${i}`} key={`folder-${i}`}>
+              <FolderHeader>
+                <FolderTrigger className="w-full text-start">
+                  <FolderHighlight>
+                    <Folder className="flex items-center gap-2 p-2 pointer-events-none">
+                      <FolderIcon
+                        closeIcon={<LucideFolderIcon className="size-4.5 text-foreground" />}
+                        openIcon={<FolderOpenIcon className="size-4.5 text-foreground" />}
+                      />
+                      <FileLabel className="text-sm">{folder.name}</FileLabel>
+                    </Folder>
+                  </FolderHighlight>
+                </FolderTrigger>
+              </FolderHeader>
 
-            <div className="relative ml-8 before:absolute before:-left-3 before:inset-y-0 before:w-px before:h-full before:bg-border">
-              <FolderContent>
-                <FileHighlight>
-                  <File className="flex items-center gap-2 p-2 pointer-events-none">
-                    <FileIcon>
-                      <LucideFileIcon className="size-4.5 text-foreground" />
-                    </FileIcon>
-                    <FileLabel className="text-sm">File 1</FileLabel>
-                  </File>
-                </FileHighlight>
-
-                <Files defaultOpen={["folder-2"]}>
-                  <FolderItem value="folder-2">
-                    <FolderHeader>
-                      <FolderTrigger className="w-full text-start">
-                        <FolderHighlight>
-                          <Folder className="flex items-center gap-2 p-2 pointer-events-none">
-                            <FolderIcon
-                              closeIcon={<LucideFolderIcon className="size-4.5 text-foreground" />}
-                              openIcon={<FolderOpenIcon className="size-4.5 text-foreground" />}
-                            />
-                            <FileLabel className="text-sm">Folder 2</FileLabel>
-                          </Folder>
-                        </FolderHighlight>
-                      </FolderTrigger>
-                    </FolderHeader>
-
-                    <div className="relative ml-8 before:absolute before:-left-3 before:inset-y-0 before:w-px before:h-full before:bg-border">
-                      <FolderContent>
-                        <FileHighlight>
-                          <File className="flex items-center gap-2 p-2 pointer-events-none">
-                            <FileIcon>
-                              <LucideFileIcon className="size-4.5 text-foreground" />
-                            </FileIcon>
-                            <FileLabel className="text-sm">File 2</FileLabel>
-                          </File>
-                        </FileHighlight>
-                        <FileHighlight>
-                          <File className="flex items-center gap-2 p-2 pointer-events-none">
-                            <FileIcon>
-                              <LucideFileIcon className="size-4.5 text-foreground" />
-                            </FileIcon>
-                            <FileLabel className="text-sm">File 3</FileLabel>
-                          </File>
-                        </FileHighlight>
-                      </FolderContent>
-                    </div>
-                  </FolderItem>
-
-                  <FileHighlight>
-                    <File className="flex items-center gap-2 p-2 pointer-events-none">
-                      <FileIcon>
-                        <LucideFileIcon className="size-4.5 text-foreground" />
-                      </FileIcon>
-                      <FileLabel className="text-sm">File 4</FileLabel>
-                    </File>
-                  </FileHighlight>
-                </Files>
-              </FolderContent>
-            </div>
-          </FolderItem>
-
-          <FileHighlight>
-            <File className="flex items-center gap-2 p-2 pointer-events-none">
-              <FileIcon>
-                <LucideFileIcon className="size-4.5 text-foreground" />
-              </FileIcon>
-              <FileLabel className="text-sm">File 5</FileLabel>
-            </File>
-          </FileHighlight>
+              <div className="relative ml-8 before:absolute before:-left-3 before:inset-y-0 before:w-px before:h-full before:bg-border">
+                <FolderContent>
+                  {folder.techs &&
+                    folder.techs.map((tech, j) => (
+                      <FileHighlight key={`file-${i}-${j}`}>
+                        <File className="flex items-center gap-2 p-2 pointer-events-none">
+                          <FileIcon>
+                            <LucideFileIcon className="size-4.5 text-foreground" />
+                          </FileIcon>
+                          <FileLabel className="text-sm">{tech}</FileLabel>
+                        </File>
+                      </FileHighlight>
+                    ))}
+                </FolderContent>
+              </div>
+            </FolderItem>
+          ))}
         </FilesHighlight>
       </Files>
     </div>
